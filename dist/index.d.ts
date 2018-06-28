151053d0bdd33fa6c5c4c42467b2927b4ab7fbd1
oscar <druiventakoscar@gmail.com>
2018-06-29 00:19:55 +0200

    remove dist folder from remote

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from "objection";
-export interface ITimestampPluginOption {
-    createdAt?: string;
-    updatedAt?: string;
-    genDate(): string | any;
-}
-export declare function timestampPlugin(options?: ITimestampPluginOption): <M extends typeof Model>(ClassModel: typeof Model) => M;
